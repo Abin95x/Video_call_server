f72f812b34f04136ec7555b947962ca93402086f
Abin <abin>
2024-06-09 14:07:55 +0530

    bugs

--- server.js
+++ server.js
@@ -1,35 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const { Server } = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3001", 
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('connected:', socket.id);
-
-//   socket.on('hello', (data) => {
-//     console.log('hello', data);
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('disconnected:', socket.id);
-//   });
-// });
-
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
