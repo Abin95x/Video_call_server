3ae92446cad3d0a1101aea921f19527db24b0660
Abin <abin>
2024-06-09 13:22:04 +0530

    git ignored

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://video-call-react-mauve.vercel.app/",
+    origin: "https://video-call-react-mauve.vercel.app",
     methods: ["GET", "POST"]
   }
 });
